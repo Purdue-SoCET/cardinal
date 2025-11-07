4577249cc0aec6db71b6f514baed8779d0f9d8a3
zachbarna <142945146+zachbarna@users.noreply.github.com>
2025-11-06 23:10:40 -0500

    regfile corrections, was indexing within banks incorrectly

--- gpu_sim/cyclesim/regfile.py
+++ gpu_sim/cyclesim/regfile.py
@@ -14,24 +14,24 @@ class RegisterFile:
         self.regs = [[[[0 for _ in range(self.threads_per_warp)] for _ in range(self.regs_per_warp)] for _ in range(self.warps // self.banks)] for _ in range(self.banks)]
 
     def write_warp_gran(self, warp_id: int, dest_operand: int, data: int) -> None:
-        self.regs[warp_id % self.banks][warp_id % (self.warps // self.banks)][dest_operand] = data
+        self.regs[warp_id % self.banks][warp_id // 2][dest_operand] = data
 
     def write_thread_gran(self, warp_id: int, dest_operand: int, thread_id: int, data: int) -> None:
-        self.regs[warp_id % self.banks][warp_id % (self.warps // self.banks)][dest_operand][thread_id] = data
-        
+        self.regs[warp_id % self.banks][warp_id // 2][dest_operand][thread_id] = data
+
     def read_warp_gran(self, warp_id: int, src_operand: int) -> Any:
-        return self.regs[warp_id % self.banks][warp_id % (self.warps // self.banks)][src_operand]
+        return self.regs[warp_id % self.banks][warp_id // 2][src_operand]
     
     def read_thread_gran(self, warp_id: int, src_operand: int, thread_id: int) -> Any:
-        return self.regs[warp_id % self.banks][warp_id % (self.warps // self.banks)][src_operand][thread_id]
+        return self.regs[warp_id % self.banks][warp_id // 2][src_operand][thread_id]
     
 ### TESTING ###
 
 regfile = RegisterFile(
     banks = 2,
-    warps = 32,
-    regs_per_warp = 64,
-    threads_per_warp = 32
+    warps = 4,
+    regs_per_warp = 4,
+    threads_per_warp = 2
 )
 
 # order of args for write (warp granularity):   (warp_id, dest_operand, data)
@@ -39,5 +39,6 @@ regfile = RegisterFile(
 # order of args for read (warp granularity):    (warp_id, src_operand, data)
 # order of args for read (thread granularity):  (warp_id, src_operand, thread_id, data)
 
-regfile.write_thread_gran(31, 2, 20, 120394234)
-print(regfile.read_warp_gran(31, 2)) 
+regfile.write_thread_gran(3, 2, 0, 120394234)
+regfile.write_warp_gran(2, 3, [67, 41])
+print(regfile.regs)
