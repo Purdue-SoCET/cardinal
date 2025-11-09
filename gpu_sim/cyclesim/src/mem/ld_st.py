75885aefdfe470f3291e0f32c439895febd1ccfb
yoshilarkins <yoshilarkins@gmail.com>
2025-11-09 13:54:53 -0500

    ld_st progress, still need to implement moving instr from ldst buffer to wb buffer

--- gpu_sim/cyclesim/src/mem/ld_st.py
+++ gpu_sim/cyclesim/src/mem/ld_st.py
@@ -1,54 +1,74 @@
+from typing import Optional
+from gpu_sim.cyclesim.src.mem import dcache
+from latch_forward_stage import Instruction, ForwardingIF, LatchIF
 
 
-from email.mime import base
-import enum
+class Ldst_Fu:
+    def __init__(self, mshr_size=4, ldst_q_size=4):
 
-
-class ldst_fu:
-    def __init__(self, name, mshr_size, ldst_q_size):
-        self.name = name
-
-        self.mshr = [None for i in range(mshr_size)]
+        self.mshr: list[Coalesce] = [None for i in range(mshr_size)]
         self.mshr_size = mshr_size
 
         self.ldst_q: list[Coalesce] = []
-        self.ldst_q_size = ldst_q_size
+        self.ldst_q_size: int = ldst_q_size
 
-        self.dcache_if = None
-        self.issue_if = None
-        self.wb_if = None
+        self.dcache_if: Optional[LatchIF] = None
+        self.issue_if: Optional[LatchIF] = None
+        self.wb_if: Optional[ForwardingIF] = None
 
         self.wb_buffer = [] #completed dcache access buffer
 
         self.outstanding = False #Outstanding dcache request addr and uid
 
-    def connect_interfaces(self, dcache_if: "StageInterface", issue_if: "StageInterface", wb_if: "StageInterface"):
+    def connect_interfaces(self, dcache_if: LatchIF, issue_if: LatchIF, wb_if: ForwardingIF):
         self.dcache_if = dcache_if
         self.issue_if = issue_if
         self.wb_if = wb_if
 
-    def tick(self, instr):
-        if len(self.ldst_q) + 1 < self.ldst_q_size:
-            self.ldst_q.append(Coalesce(instr))
-        
-        if len(self.ldst_q) == self.ldst_q_size:
-            self.issue_if.push({"full": 1}) # backpressure
-        
-        if self.outstanding == False:
-            coalesce: Coalesce = self.ldst_q[0]
-            addr = coalesce.genRequestAddr()
-            mode = coalesce.write
-            uid = 0
-        else:
-            if dcache_if.hit:
-                if dcache_if.data.uid == 1:
-                    
+    def tick(self):
 
-                self.outstanding = False
+        #populate ldst_q if not full
+        if len(self.ldst_q) + 1 < self.ldst_q_size:
+            instr = self.issue_if.pop()
+            if instr != None:
+                self.ldst_q.append(Coalesce(instr))
+
+        #apply backpressure if full
+        if len(self.ldst_q) == self.ldst_q_size:
+            self.issue_if.forward_if.set_wait(True)
+        else:
+            self.issue_if.forward_if.set_wait(False)
+
+        #Push cache access if no outstanding
+        if self.outstanding == False and self.dcache_if.ready_for_push():
+            coal: Coalesce = self.ldst_q[0]
+            addr = coal.genRequestAddr()
+            mode = coal.write
+            self.outstanding = True
+
+            self.dcache_if.push(
+                {
+                    "instr":coal.instr
+                    "addr": addr,
+                    "mode": mode
+                }
+            )
+        elif self.dcache_if.forward_if.payload["hit"] == True:
+            payload = self.dcache_if.forward_if.pop()
+            self.outstanding = False
+            coal: Coalesce = self.ldst_q[0]
+            coal.parseHit(payload)
+
+        elif self.dcache_if.forward_if.payload["miss"] == True:
+            payload = self.dcache_if.forward_if.pop()
+            if len(self.wb_buffer) != 0:
+                self.wb_buffer[0].parseHit(payload)
+            else:
+                self.ldst_q[0].parseHit(payload)
         
 
 class Coalesce():
-    cache_line_mask = 0xFF_FF_FF_FF_FF_FF_FF_00
+    cache_line_mask = 0xFF_FF_FF_FF_FF_FF_FC_00
     cache_fetch_step = 0x4
 
     def __init__(self, instr):
@@ -57,7 +77,8 @@ class Coalesce():
         self.addrs = [] # initialized with all addr mem addrs
         self.data = []
         self.pending_addrs = []
-        self.in_flight_idx = [0 for i in range(32)]
+        self.finished_idxs = [0 for i in range(32)]
+        self.mshr_addrs = []
         self.in_flight_addr = 0
 
     def inRange(self, base_addr, addr):
@@ -69,7 +90,7 @@ class Coalesce():
         pass
 
     def genRequestAddr(self) -> int:
-        self.in_flight_addr = self.pending_addrs[0] & coalesce.cache_line_mask
+        self.in_flight_addr = self.pending_addrs[0] & Coalesce.cache_line_mask
         coal_addrs = [addr for addr in self.pending_addrs if self.inRange(self.in_flight_addr, addr)]
         for idx, addr in self.addrs:
             if addr in coal_addrs:
@@ -77,16 +98,12 @@ class Coalesce():
         return self.in_flight_addr
 
     def parseHit(self, data):
-        if len(data) != 32:
-            print("Err: data is not of right size")
-            print(data)
         data_addr = self.in_flight_addr
         for i, d in enumerate(data):
             if self.in_flight_idx[i]:
                 self.data[i] = d
-            data_addr += coalesce.cache_fetch_step
-        
+            data_addr += Coalesce.cache_fetch_step
         self.in_flight_idx = [0 for i in range(32)]
     
     def readyWB(self):
-        return len(self.pending_addrs) == 0 and not any(self.in_flight_idx)
+        return all(self.finished_idxs)
