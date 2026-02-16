f9893e3bd1282019039fbd1af09505b127ce1bf1
Alec Andrulis <aandruli@purdue.edu>
2026-02-16 16:47:02 -0500

    Implemented basic texture mapping into pipeline

---
+++ graphics_hw/texture_unit.py
@@ -0,0 +1,86 @@
+import numpy as np
+from PIL import Image
+
+class texture_unit():
+    def __init__(self, textures):
+        self.textures = textures
+
+
+    # def tex_map(self, uv, texture):
+    #
+    #     # uv: len : h * w * 2, [0] = u, [1] = v
+    #     #texture: len : h * w
+    #
+    #     #values are row, column starting in top left
+    #
+    #     out = np.zeros((uv.shape[0],uv.shape[1],3), dtype=np.float32)
+    #
+    #     for row in range(len(texture)):
+    #         for col in range(len(texture[row])):
+    #
+    #             if texture[row][col] != -1:
+    #                 current_texture = self.textures[int(texture[row][col]-1)]
+    #                 tex_width = current_texture.shape[1]
+    #                 tex_height = current_texture.shape[0]
+    #
+    #                 u_scaled = int(uv[row][col][0] *(tex_width-1))
+    #                 v_scaled = int(uv[row][col][1] *(tex_height-1))
+    #
+    #                 out[row][col] = current_texture[v_scaled][u_scaled]
+    #     return out
+
+    def tex_map(self, uv, texture):
+
+        # uv: len : h * w * 2, [0] = u, [1] = v
+        #texture: len : h * w
+
+        #values are row, column starting in top left
+
+        out = np.zeros((uv.shape[0],uv.shape[1],3), dtype=np.float32)
+
+
+        for i in range(len(self.textures)):
+            current_texture = self.textures[i]
+            tex_width = current_texture.shape[1]
+            tex_height = current_texture.shape[0]
+
+            mask = (texture == i+1)
+
+            #extract masked uvs
+            uv_masked = uv[mask]
+            u_scaled = (uv_masked[:,0] * (tex_width-1)).astype(int)
+            v_scaled = (uv_masked[:,1] * (tex_height-1)).astype(int)
+
+            out[mask] = current_texture[v_scaled,u_scaled]
+
+        return out
+
+
+
+
+
+
+
+
+        # for row in range(len(texture)):
+        #     for col in range(len(texture[row])):
+        #
+        #         if texture[row][col] != -1:
+        #             current_texture = self.textures[int(texture[row][col]-1)]
+        #             tex_width = current_texture.shape[1]
+        #             tex_height = current_texture.shape[0]
+        #
+        #             u_scaled = int(uv[row][col][0] *(tex_width-1))
+        #             v_scaled = int(uv[row][col][1] *(tex_height-1))
+        #
+        #             out[row][col] = current_texture[v_scaled][u_scaled]
+        # return out
+
+
+
+
+
+
+
+
+
