8801f66e1081acdafc271436e17b122931e251c7
SethThMc <smcconkey15@gmail.com>
2026-03-04 16:37:14 -0500

    initial performance counter implementation

--- gpu/src/simulator/utils/performance_counter/__init__.py
+++ gpu/src/simulator/utils/performance_counter/__init__.py
@@ -1,8 +1,5 @@
-"""
-Performance counters for GPU simulator.
-"""
+from simulator.utils.performance_counter.perf_counter_base import PerfCounterBase
+from simulator.utils.performance_counter.perf_config import PerfConfig, FlightRecorderConfig
+from simulator.utils.performance_counter.telemeter import Telemeter
 
-from .execute import ExecutePerfCount
-from .writeback import WritebackPerfCount
-
-__all__ = ['ExecutePerfCount', 'WritebackPerfCount']
+__all__ = ["PerfCounterBase", "PerfConfig", "FlightRecorderConfig", "Telemeter"]
