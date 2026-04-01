7d67596bb43e084183ce1dddbff3244ba6a4cb02
Crypto <chen4880@purdue.edu>
2026-04-01 19:02:28 -0400

    benchmark: cpu main files updates; Graphics: colored cube with lighting benchmark

--- benchmark/kernels/include/kmeans.h
+++ benchmark/kernels/include/kmeans.h
@@ -2,12 +2,12 @@
 #define KMEANS_H
 
 typedef struct {
-	int n_points;
-	int n_dims;
-	int k;
-	const float* points;
-	const float* centers;
-	int* labels;
+	int n_points; //number of points
+	int n_dims; //number of dimensions for each point
+	int k; //number of clusters
+	const float* points; //array of points 
+	const float* centers; //array of cluster centers
+	int* labels; //cluster label for each point
 } kmeans_assign_arg_t;
 
 typedef struct {
@@ -16,8 +16,8 @@ typedef struct {
 	int k;
 	const float* points;
 	const int* labels;
-	float* center_sums;
-	int* center_counts;
+	float* center_sums; //sum of points for each cluster
+	int* center_counts; //number of points assigned to each cluster
 } kmeans_accum_arg_t;
 
 typedef struct {
