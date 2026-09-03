9392789c261caa31793a116f00609209cccecffc
SiddhanthGH <110147711+SiddhanthGH@users.noreply.github.com>
2026-09-03 01:12:36 -0400

    Added bcull, partial ladder stop ops, input control, real-time rendering, orbital camera.

--- graphics_hw/main.py
+++ graphics_hw/main.py
@@ -5,35 +5,94 @@ from PIL import Image
 from texture_unit import  texture_unit
 from calculate_gradients import process_tmu_quads
 import matplotlib.pyplot as plt
+import keyboard
 
-'''
-v0 = (-0.8, -0.8, -2)  # bottom left
-v1 = ( 0.8, -0.8, -2)  # bottom right
-v2 = ( 0.8,  0.8, -2)  # top right
-v3 = (-0.8,  0.8, -2)  # top left
+def obj_to_soa_rasterizer(file_path):
+    vertices = []
+    triangles_vs = []
+    face_lines = []
+    
+    # PASS 1: Accumulate all physical 3D vertices
+    with open(file_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+                
+            parts = line.split()
+            if not parts:
+                continue
+                
+            if parts[0] == 'v':
+                vertices.append([float(parts[1]), float(parts[2]), float(parts[3])])
+            elif parts[0] == 'f':
+                face_lines.append(parts[1:])
 
-# Colors per corner
-c0 = [1,0,0]  # red
-c1 = [0,1,0]  # green
-c2 = [0,0,1]  # blue
-c3 = [1,1,1]  # white
+    # PASS 2: Reconstruct triangles 
+    for face_data in face_lines:
+        face_indices = []
+        for p in face_data:
+            idx = int(p.split('/')[0])
+            if idx > 0:
+                face_indices.append(idx - 1)
+            else:
+                face_indices.append(len(vertices) + idx)
+        
+        # Split any quads/polygons into triangles
+        for i in range(1, len(face_indices) - 1):
+            v0 = vertices[face_indices[0]]
+            v1 = vertices[face_indices[i]]
+            v2 = vertices[face_indices[i + 1]]
+            triangles_vs.append([v0, v1, v2])
 
-vx1 = [v0[0], v1[0]]
-vy1 = [v0[1], v1[1]]
-vz1 = [v0[2], v1[2]]
+    # --- Generate the Structure of Arrays (SoA) Colors ---
+    col1 = []  # Colors for the 1st vertex of every triangle
+    col2 = []  # Colors for the 2nd vertex of every triangle
+    col3 = []  # Colors for the 3rd vertex of every triangle
+    
+    palette = [
+        [[1,0,0], [0,0,1], [1,1,1]], 
+        [[0,1,0], [0,1,0], [1,1,1]], 
+        [[0,0,1], [1,0,0], [1,1,1]]  
+    ]
+    
+    for i in range(len(triangles_vs)):
+        current_tri_colors = palette[i % len(palette)]
+        
+        col1.append(current_tri_colors[0])
+        col2.append(current_tri_colors[1])
+        col3.append(current_tri_colors[2])
 
-vx2 = [v1[0], v3[0]]
-vy2 = [v1[1], v3[1]]
-vz2 = [v1[2], v3[2]]
+    # Return everything cleanly as a tuple
+    return triangles_vs, col1, col2, col3
 
-vx3 = [v3[0], v2[0]]
-vy3 = [v3[1], v2[1]]
-vz3 = [v3[2], v2[2]]
+# v0 = (-0.8, -0.8, -2)  # bottom left
+# v1 = ( 0.8, -0.8, -2)  # bottom right
+# v2 = ( 0.8,  0.8, -2)  # top right
+# v3 = (-0.8,  0.8, -2)  # top left
 
-col1 = [c0, c1]
-col2 = [c1, c3]
-col3 = [c3, c2]
-'''
+# # Colors per corner
+# c0 = [1,0,0]  # red
+# c1 = [0,1,0]  # green
+# c2 = [0,0,1]  # blue
+# c3 = [1,1,1]  # white
+
+# vx1 = [v0[0], v1[0]]
+# vy1 = [v0[1], v1[1]]
+# vz1 = [v0[2], v1[2]]
+
+# vx2 = [v1[0], v3[0]]
+# vy2 = [v1[1], v3[1]]
+# vz2 = [v1[2], v3[2]]
+
+# vx3 = [v3[0], v2[0]]
+# vy3 = [v3[1], v2[1]]
+# vz3 = [v3[2], v2[2]]
+
+# col1 = [c0, c1]
+# col2 = [c1, c3]
+# col3 = [c3, c2]
+
 
 #I recommend sticking to this test case. It's relatively simple while also testing z and top left rule well enough.
 #The test above is extremely strict for msaa, even hardware is expected to fail it at lower resolutions with msaa. Use at your own discretion.
@@ -54,17 +113,17 @@ col3 = [c3, c2]
 
 #Test case for texture mapping
 
-vs = [[[-8.5, 1, -20], [-8.5, -1, -1], [8.5, -1, -1]], #triangle 1
-        [[-8.5, 1, -20], [8.5, 1, -20], [8.5, -1, -1]]] #triangle 2
+# vs = [[[-8.5, 1, -20], [-8.5, -1, -1], [8.5, -1, -1]], #triangle 1
+#         [[-8.5, 1, -20], [8.5, 1, -20], [8.5, -1, -1]]] #triangle 2
 
-tex_id = [0,0,0]
+# tex_id = [0,0,0]
 
-u = [[0,0,1], [0,1,1]]
-v = [[0,1,1],[0,0,1]]
+# u = [[0,0,1], [0,1,1]]
+# v = [[0,1,1],[0,0,1]]
 
-col1 = [[1,1,1], [1,1,1]]
-col2 = [[1,1,1], [1,1,1]]
-col3 = [[1,1,1], [1,1,1]]
+# col1 = [[1,1,1], [1,1,1]]
+# col2 = [[1,1,1], [1,1,1]]
+# col3 = [[1,1,1], [1,1,1]]
 
 
 '''
@@ -130,14 +189,16 @@ invalid state : NA
 After running above you can run rasterEngine.showScreen() and it will show you your image with the texture map applied. 
 '''
 
-# rasterEngine = Rasterizer(
-#     vs, col1, col2, col3, msaa = 2
-# ) #defaults to 720p, msaa = 0, near = 1, far = 10
+vs, col1, col2, col3 = obj_to_soa_rasterizer("torus.obj")
 
 rasterEngine = Rasterizer(
-    vs, col1, col2, col3,
-    u = u, v = v, tex_id = tex_id, msaa=0
-)
+    vs, col1, col2, col3, msaa = 0, zoffset = 0, bcull = 1, rotate = 1, radius = 3, angle = 0, w=300, h=300
+) #defaults to 720p, msaa = 0, near = 1, far = 10
+
+# rasterEngine = Rasterizer(
+#     vs, col1, col2, col3,
+#     u = u, v = v, tex_id = tex_id, msaa=0
+# )
  #defaults to 720p, msaa = 0, near = 1, far = 10
 
 
@@ -147,26 +208,39 @@ rasterEngine = Rasterizer(
 # image_array = image_array/255.0
 
 #must be saved as PNGs
-tex_names = ["debug_colors"]
+#tex_names = ["debug_colors"]
 
 
-tex_unit = texture_unit(tex_names)
-
-
-
+#tex_unit = texture_unit(tex_names)
 
 
 
-start = perf_counter()
-rasterEngine.render()
-end = perf_counter()
+# start = perf_counter()
+# rasterEngine.render()
+# end = perf_counter()
+
+while True:
+    start = perf_counter()
+    rasterEngine.clearScreen()
+
+    if (keyboard.is_pressed('d')):
+        rasterEngine.changeAngle(5)
+    elif (keyboard.is_pressed('a')):
+        rasterEngine.changeAngle(-5)
+    elif (keyboard.is_pressed('q')):
+        break
+
+    rasterEngine.render()
+    rasterEngine.updateScreen()
+    end = perf_counter()
+    print("Time to render:", end - start)
 
 
-partials = process_tmu_quads(rasterEngine.getSamples(),rasterEngine.getUV())
+#partials = process_tmu_quads(rasterEngine.getSamples(),rasterEngine.getUV())
 
 
 
-out = tex_unit.tex_map(rasterEngine.getUV(),rasterEngine.getSamples(), partials, filtering = 2, mipmap=True)
+#out = tex_unit.tex_map(rasterEngine.getUV(),rasterEngine.getSamples(), partials, filtering = 2, mipmap=True)
 
 
 # # code for rendering teapot, comment out raster engine calls to use this
@@ -181,10 +255,10 @@ out = tex_unit.tex_map(rasterEngine.getUV(),rasterEngine.getSamples(), partials,
 # # plt.show()
 #
 #
-rasterEngine.applyTextures(out)
+#rasterEngine.applyTextures(out)
 #
 #
 #
-print("Time to render:", end - start)
+# print("Time to render:", end - start)
 #
-rasterEngine.showScreen()
+# rasterEngine.showScreen()
--- graphics_hw/rasterizer.py
+++ graphics_hw/rasterizer.py
@@ -2,20 +2,27 @@ import numpy as np
 from graphics_lib import Triangle, Projector, Vertex
 import matplotlib.pyplot as plt
 from PIL import Image
+import math
 
 #Set up machines
 class Rasterizer():
+
+    def changeAngle(self, angle):
+        self.angle += angle
 
     def getNDC(self, vertex : Vertex):
         near_point = self.projector.toNearPlane(vertex)
         ndc : Vertex = self.projector.toNDC(near_point)
         return ndc
 
-    def project(self, vert):
+    def project(self, vert : Vertex):
+
+        ogvert = vert
+
         #Convert to NDC
         ndc = self.getNDC(vert)
         out = self.projector.toScreenSpace(ndc)
-        out.z = self.projector.depth(out)
+        out.z = self.projector.depth(ogvert)
 
         #Convert to screen space
         return out
@@ -24,9 +31,9 @@ class Rasterizer():
         return (b.x - a.x) * (y - a.y) - (b.y - a.y) * (x - a.x)
 
     def validEdge (self, v0, v1):
-        edge_vec = [v1.x - v0.x, v1.y - v0.y]
-        top = edge_vec[1] == 0 and edge_vec[0] < 0  
-        left = edge_vec[1] < 0                      
+        edge_vec = [v1.x - v0.x, v1.y - v0.y] #1 cycle
+        top = edge_vec[1] == 0 and edge_vec[0] < 0   #2 cycle
+        left = edge_vec[1] < 0 #2 cycle
         return top or left
     
     def de_dx(self, a, b):
@@ -34,7 +41,7 @@ class Rasterizer():
     def de_dy(self, a, b):
         return b.x - a.x
 
-    def __init__(self, vs = [[[]]], col1 = [[]], col2 = [[]], col3 = [[]], u = [[-1, -1, -1]], v = [[-1, -1, -1]], msaa = 0, w=1280, h=720, near = 1, far = 10, tex_id = [-1]):
+    def __init__(self, vs = [[[]]], col1 = [[]], col2 = [[]], col3 = [[]], u = [[-1, -1, -1]], v = [[-1, -1, -1]], msaa = 0, w=1280, h=720, near = 1, far = 10, tex_id = [-1], zoffset = 0, bcull = 0, rotate = 0, radius = 0, angle = 0):
         msaa = 2 if (msaa > 2) else msaa
         
         #numpy arrays, all of them. Cols will be nx3x3 array
@@ -50,6 +57,14 @@ class Rasterizer():
         self.msaa = msaa
         self.w = w
         self.h = h
+        self.bcull = bcull
+        self.rot = rotate
+        self.radius = radius
+        self.angle = angle
+        self.near = near
+        self.far = far
+
+        self.zoffset = zoffset
 
         self.tex_ids = tex_id * len(vs) if tex_id[0] == -1 else tex_id #The sample name or number whatever you want for each triangle. len = no. triangles
 
@@ -64,6 +79,14 @@ class Rasterizer():
         return self.uv_buffer
     def getSamples(self):
         return self.sampleId_buffer
+
+    def clearScreen(self):
+        self.projector = Projector(self.w, self.h, self.near, self.far) #width, height, near plane, far plane
+        self.screen = np.zeros((self.h,self.w,3))
+        self.z_buffer = np.full((self.h,self.w,self.msaa if self.msaa > 0 else 1),np.inf)
+        self.color_buffer = np.zeros((self.h,self.w,self.msaa if self.msaa > 0 else 1,3))
+        self.uv_buffer = np.full((self.h,self.w,2),np.nan) #if there is a valid uv to be applied for a pixel then np.isfinite(u) and np.isfinite(v)
+        self.sampleId_buffer = (np.ones((self.h,self.w)) * -1).astype(int) #The sample you should be pulling from for each pixel.
 
     def applyTextures(self, newRGB : np.array):
         #Modulate math, just uses original interpolated color as albedo to apply to texture. 
@@ -94,13 +117,36 @@ class Rasterizer():
         self.render_2msaa() if self.msaa == 2 else self.render_0msaa()
 
     def render_2msaa(self): #Attribute interpolator + rasterizer
+
+        angle = math.radians(self.angle)
+
         for i in range(0,len(self.vs)):
+
+            x1, y1, z1 = self.vs[i][0]
+            x2, y2, z2 = self.vs[i][1]
+            x3, y3, z3 = self.vs[i][2]
+
+            if (self.rot != 0):
+                # Rotate on the X-Axis (Pitch tumble)
+                y1_temp = y1 * math.cos(-angle) - z1 * math.sin(-angle)
+                z1 = y1 * math.sin(-angle) + z1 * math.cos(-angle) - self.radius
+                y1 = y1_temp
+
+                y2_temp = y2 * math.cos(-angle) - z2 * math.sin(-angle)
+                z2 = y2 * math.sin(-angle) + z2 * math.cos(-angle) - self.radius
+                y2 = y2_temp
+
+                y3_temp = y3 * math.cos(-angle) - z3 * math.sin(-angle)
+                z3 = y3 * math.sin(-angle) + z3 * math.cos(-angle) - self.radius
+                y3 = y3_temp
+
+
 
             #Get primitives
 
-            vertex1 = Vertex(self.vs[i][0][0], self.vs[i][0][1], self.vs[i][0][2], self.col1[i], self.u[i][0], self.v[i][0])
-            vertex2 = Vertex(self.vs[i][1][0], self.vs[i][1][1], self.vs[i][1][2], self.col2[i], self.u[i][1], self.v[i][1])
-            vertex3 = Vertex(self.vs[i][2][0], self.vs[i][2][1], self.vs[i][2][2], self.col3[i], self.u[i][2], self.v[i][2])
+            vertex1 = Vertex(x1, y1, z1 + self.zoffset, self.col1[i], self.u[i][0], self.v[i][0])
+            vertex2 = Vertex(x2, y2, z2 + self.zoffset, self.col2[i], self.u[i][1], self.v[i][1])
+            vertex3 = Vertex(x3, y3, z3 + self.zoffset, self.col3[i], self.u[i][2], self.v[i][2])
 
             triangle = Triangle(vertex1, vertex2, vertex3)
 
@@ -120,10 +166,15 @@ class Rasterizer():
             ss_max.y = max(0, min(self.h - 1, int(ss_max.y)))
 
             #Check winding.
-            area = self.edge(ss_tri.A, ss_tri.B, ss_tri.C.x, ss_tri.C.y) / 2 #Edge equation is cross product, so divide by 2 to get area of triangle instead of parallelogram.
-            if area < 0:
+            area = self.edge(ss_tri.A, ss_tri.B, ss_tri.C.x, ss_tri.C.y) / 2 #Edge equation is cross product, so divide by 2 to get area of triangle instead of parallelogram
+
+            if (area > 0 and self.bcull == 0):
                 ss_tri.B, ss_tri.C = ss_tri.C, ss_tri.B
                 area = -area
+
+            if area > 0 and self.bcull != 0:
+                continue
+
             norm_factor = 1 / (2 * area)
 
             
@@ -294,13 +345,35 @@ class Rasterizer():
                 e_ini3 += de_dy3
                     
     def render_0msaa(self): #Attribute interpolator + rasterizer
+
+        angle = math.radians(self.angle)
+
         for i in range(0,len(self.vs)):
+            x1, y1, z1 = self.vs[i][0]
+            x2, y2, z2 = self.vs[i][1]
+            x3, y3, z3 = self.vs[i][2]
+
+            if (self.rot != 0):
+                # Rotate on the X-Axis (Pitch tumble)
+                y1_temp = y1 * math.cos(-angle) - z1 * math.sin(-angle)
+                z1 = y1 * math.sin(-angle) + z1 * math.cos(-angle) - self.radius
+                y1 = y1_temp
+
+                y2_temp = y2 * math.cos(-angle) - z2 * math.sin(-angle)
+                z2 = y2 * math.sin(-angle) + z2 * math.cos(-angle) - self.radius
+                y2 = y2_temp
+
+                y3_temp = y3 * math.cos(-angle) - z3 * math.sin(-angle)
+                z3 = y3 * math.sin(-angle) + z3 * math.cos(-angle) - self.radius
+                y3 = y3_temp
+
+
 
             #Get primitives
 
-            vertex1 = Vertex(self.vs[i][0][0], self.vs[i][0][1], self.vs[i][0][2], self.col1[i], self.u[i][0], self.v[i][0])
-            vertex2 = Vertex(self.vs[i][1][0], self.vs[i][1][1], self.vs[i][1][2], self.col2[i], self.u[i][1], self.v[i][1])
-            vertex3 = Vertex(self.vs[i][2][0], self.vs[i][2][1], self.vs[i][2][2], self.col3[i], self.u[i][2], self.v[i][2])
+            vertex1 = Vertex(x1, y1, z1 + self.zoffset, self.col1[i], self.u[i][0], self.v[i][0])
+            vertex2 = Vertex(x2, y2, z2 + self.zoffset, self.col2[i], self.u[i][1], self.v[i][1])
+            vertex3 = Vertex(x3, y3, z3 + self.zoffset, self.col3[i], self.u[i][2], self.v[i][2])
 
             triangle = Triangle(vertex1, vertex2, vertex3)
 
@@ -321,9 +394,14 @@ class Rasterizer():
 
             #Check winding.
             area = self.edge(ss_tri.A, ss_tri.B, ss_tri.C.x, ss_tri.C.y) / 2 #Edge equation is cross product, so divide by 2 to get area of triangle instead of parallelogram.
-            if area < 0:
+
+            if area > 0 and self.bcull == 0:
                 ss_tri.B, ss_tri.C = ss_tri.C, ss_tri.B
                 area = -area
+
+            if (area > 0 and self.bcull != 0):
+                continue
+            
             norm_factor = 1 / (2 * area)
                 
             bx = ss_min.x + 0.5
@@ -351,6 +429,8 @@ class Rasterizer():
                 edge1 = e_ini1
                 edge2 = e_ini2
                 edge3 = e_ini3
+
+                flag = 0
                     
                 for j in range(ss_min.x, ss_max.x):
                     if area == 0:
@@ -360,6 +440,7 @@ class Rasterizer():
                     w = ss_tri.A.w * edge1 + ss_tri.B.w * edge2 + ss_tri.C.w * edge3
 
                     if ((edge1 + check1 >= 0) and (edge2 + check2 >= 0) and (edge3 + check3 >= 0) and z <= self.z_buffer[y][j][0]):
+                        flag = 1
                         self.screen[y][j][0] = ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3
                         self.screen[y][j][1] = ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3
                         self.screen[y][j][2] = ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3
@@ -373,6 +454,9 @@ class Rasterizer():
                     edge2 += de_dx2
                     edge3 += de_dx3
 
+                    if (flag == 1 and ((edge1 + check1 < 0) or (edge2 + check2 < 0) or (edge3 + check3 < 0))):
+                        break
+
                 e_ini1 += de_dy1
                 e_ini2 += de_dy2
                 e_ini3 += de_dy3    
@@ -381,6 +465,23 @@ class Rasterizer():
         plt.imshow(np.clip(self.screen, 0.0, 1.0))
         plt.axis('off')
         plt.show()
+
+    def updateScreen(self):
+        plt.ion()
+        
+        # 1. Update or create the image element
+        if plt.gca().images:
+            plt.gca().images[0].set_data(np.clip(self.screen, 0.0, 1.0))
+        else:
+            plt.imshow(np.clip(self.screen, 0.0, 1.0))
+            plt.axis('off')
+        
+        # 2. Force an IMMEDIATE visual update to the canvas data
+        plt.gcf().canvas.draw()
+        
+        # 3. Process events and briefly pause to allow the GUI to repaint
+        plt.gcf().canvas.flush_events()
+        plt.pause(0.001)  # Critical for frame pacing
 
     def saveScreen(self, filename = 'lossless.png'):
         #Save image
